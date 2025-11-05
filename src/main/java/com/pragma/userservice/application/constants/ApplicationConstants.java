7c6138a0e625eb068862c5860be9409966eeee68
LoperaUr <manuellopera612@gmail.com>
2025-11-04 19:53:53 -0500

    feat: add application and domain constants for user validation

---
+++ src/main/java/com/pragma/userservice/application/constants/ApplicationConstants.java
@@ -0,0 +1,17 @@
+package com.pragma.userservice.application.constants;
+
+public final class ApplicationConstants {
+
+    // Prevent instantiation
+    private ApplicationConstants() {}
+
+    public static final String NAME_CANNOT_BE_BLANK = "Name cannot be blank";
+    public static final String LAST_NAME_CANNOT_BE_BLANK = "Last name cannot be blank";
+    public static final String IDENTIFICATION_NUMBER_CANNOT_BE_BLANK = "Identification number cannot be blank";
+    public static final String PHONE_NUMBER_CANNOT_BE_BLANK = "Phone number cannot be blank";
+    public static final String BIRTH_DATE_CANNOT_BE_NULL = "Birth date cannot be null";
+    public static final String EMAIL_SHOULD_BE_VALID = "Email should be valid";
+    public static final String EMAIL_CANNOT_BE_BLANK = "Email cannot be blank";
+    public static final String PASSWORD_CANNOT_BE_BLANK = "Password cannot be blank";
+
+}
---
+++ src/main/java/com/pragma/userservice/domain/constants/DomainConstants.java
@@ -0,0 +1,15 @@
+package com.pragma.userservice.domain.constants;
+
+public final class DomainConstants {
+
+    // Prevent instantiation
+    private DomainConstants() {}
+
+    public static final String MSG_USER_NOT_FOUND = "User not found";
+    public static final String MSG_INVALID_CELLPHONE = "Invalid cellphone number";
+    public static final String MSG_INVALID_DOCUMENT = "Invalid document number";
+    public static final String MSG_UNDERAGE_USER = "User must be at least 18 years old";
+    public static final String MSG_EMAIL_ALREADY_EXISTS = "Email already exists";
+    public static final String MSG_PHONE_ALREADY_EXISTS = "Phone number already exists";
+
+}
